4dfb9b52f7f7b86e36a90d1ba815748700c3f1fa
vishalg8454 <vishalgaurav@Vishals-MacBook-Pro.local>
2024-12-30 21:21:29 +0530

    add solution

---
+++ day-15/part-two.py
@@ -0,0 +1,250 @@
+# wrong solution, couldn't solve it
+
+map_str='''##################################################
+#.#...........O.O....#..O.#O.O#.O....OOO.O...O...#
+#.#O......O...O..#...........#...#.....#...##....#
+#O.OO..O.OO.O..OO.....O...#...O...#O.O.#..O...##.#
+#...OOO..#..OO.....O.O.#OO#.OO#....O.......#..OO##
+#.O......OO.O.O...OOO.O#......##..O.O..O......O###
+##.......#.O.OO..O.....O....#..........O..O.O...O#
+#.....OO..##.O.#...#....#......OO.....O.O...O....#
+#OO.OOO...#..#O.....O#O...O.#...........O..#..##O#
+#OO##......O.O.#OO.O.OO..#O.O.O.O.O.O.....O.OOO.O#
+#O.....#OOO...O.O...#.#O....#......OOO...O.OO....#
+#O.O...O.........#..##..O..#.O...O...O..O.OO..#.O#
+#.......O..O#....OO.O.O#...O..O.#O..O.O#.....OO..#
+#OO.O#O...O#..#...#O.#....O#O..#.O..O#...O..##..O#
+#O......O.#..O.......O.O.OO..O#.......O..O#..#..O#
+#....O...OO...O.O..##....O.#..O.O..O..O....OOOO..#
+#.....O.O.O..O.O.O.O.O.OO....O..OO..O.O...O.O#..O#
+#O......OO.O......#O.O.OO#......O....OO..O...O..O#
+#O......OO..O....O...O.....OOO.O....OO.O..O#...OO#
+#..OOOOOO.O..OO..O...O.....#......OO.#.O...#O.O.O#
+#...O..#O..O.OO.......O...##OO.#.........#O.#...O#
+#...OO.#O..OO....##....#...OO..#...O.O..O...O..OO#
+##...O.#..#O.#OO......O..OO.O#..O.....O.#..O.OOOO#
+##.....#......O.OO#O.O..O....O..O.OOO.O..O...O.#O#
+#........O.O..OO..O.....@....#..O...OOOO#.O......#
+#O....O....O...OO.OOO...O.OO.....OO#..O......O.O.#
+#...O..O...O.OOOO.....#...O.O....O##O.....O.#...O#
+#...O.....O...#.......O.OO..O..#.#OO.OO........#.#
+##.O........#.......#O#.............O...O#..#.O#.#
+#O.....O.OOOO.....O.O...OO.O...O.O...OO..O...O.O.#
+#...........O...OOOOOO....O.O.#.OO...O.O..OO.....#
+#..O##.......OO.OO.##..#..O...#.O..O#O...O.O#..O.#
+#...OO...OO.O.......#.....OOO.O..O..O.....O...O..#
+#..OO.......OO.....#......OO.O..O..#....OO....O..#
+#...OO..OO...O.OO.O#.O...#.O..........OOO...OO..##
+#..#O..O#....O....#....O.#...OO.O.#...#.........O#
+#O.......O..OO...O.O.O...OO#O.#..O.#O..#..O.#O..O#
+#..O..O...#OO...O..O.#OO.O.O...O.......##..O#..#.#
+#.O..#....O..O.O..O..O.O..#....O..#...O.OO#.O....#
+##OOO.O.O.................O.O..##OO.O.O.O.OO.O.O.#
+#..O..................OO...O....OOO#...O....O.#..#
+#..#.O#..#...OO....#....OO......#......O..OO..O.O#
+#.O..OO...OO.#.OO..OOOOO#.O.OO...O.O.#...O..#.OO.#
+#...O..#O#...#..............OO.#.#.....OO.......O#
+#.O#.....O#..OO.#..O......#..#O.O#.O.OO........O.#
+#OO.O#...O#.O......OO......O...........##.O..#...#
+#..O#...O.#...O.O...O...OO.O#OOO..OO#..O..OOOO.O.#
+#...O.O#..OO........O#.OO.....#O.O.O#......#O.O#.#
+#........O....O....O#..#.....O..O..OO#.OOO....O..#
+##################################################'''
+
+# moves = '''<vv<<^^<<^^'''
+# moves = '''<vv'''
+moves='''^v<v^^^vv><<><><<^><><>vv><<v^^<<>^^v><>v><^^^vvv^^><<^v^>^^<>><v<vv>v>v<^<
+^^>v^v<v^>>^vv^v><>>v<v>v>^^><>^>v<vv><^^><>^^^^<<<v^>v>^<vv<<>>v><v^vvv>v^v>^<>^v^vv
+^<^^<<<vv^<^<^v><<<^v<vvv^v^><>>v^vv^^>^^^^><<<^^^v<^^v>^^vv<>><<>v>v<^^v>v^^<<vv>^>^
+^<<>^<vv<^v^<^^^>>^<<<^vv^<v>>>v<><>^^><^>><v<<<>^^^vv^v<>v^v<^v^vv<<vv><v>^<^<<>v<^<
+<><^<>><^<>^v^><<^<<^>><>>><^vv<>vv<^>v><>vvvvv<><>>v>>^vvv^v<><<^><>^<v^><<>>vv<<>>>
+>v<<>v<><vv>vv>^v<v^<<<v<^>>>^v^<^<<<v<v^v>>v<>^<vv>>^^>^vv>v^^>^vvv<^^vv^^<v^>^>vv<^<
+v<>^><v>vv<<v>^^<<>v^vvv^<^><^<>^>^^^<<><v>^vv<<><>^^<><^vv<v^<>>^^^>^<<^^<><<<^^v><>^
+<^<v>v>>^>^^v<>^v^<^v^^v>>v<^>>><v^v^^>>>vv^<v^^>v>^>><v>v^><^<vv<<<<v^^^<>v>^v<^^<><^
+v>><v>^>>>^v^^<<^^^>v>v^^v^>>^<v^vvv>^<^v<v<<^<<v<>v<^>vv^<v><^<v^<>>v<>v<<<<^<v<>v^^<
+'''
+
+new_str=""
+for i in map_str:
+    if i == "#":
+        new_str+="##"
+    elif i == "O":
+        new_str += "[]"
+    elif i == ".":
+        new_str+=".."
+    elif i == "@":
+        new_str+="@."
+    else:
+        new_str+=i
+
+ar = new_str.split("\n")
+arr = [list(x) for x in ar]
+x=len(ar)
+y=len(ar[0])
+
+def get_bot_pos():
+    for i in range(x):
+        for j in range(y):
+            if arr[i][j] == "@":
+                return (i,j)
+
+def print_arr():
+    for i in range(x):
+        for j in range(y):
+            print(arr[i][j],end="")
+        print()
+    print()
+
+def shift_right(bot_x,bot_y):
+    right=bot_y
+    while arr[bot_x][right+2] == ']':
+        right+=2
+    if arr[bot_x][right+1] == ".":
+        for i in range(right+1,bot_y,-1):
+            arr[bot_x][i]=arr[bot_x][i-1]
+        arr[bot_x][bot_y]="."
+        
+def shift_left(bot_x,bot_y):
+    left=bot_y
+    while arr[bot_x][left-2] == "[":
+        left-=2
+    if arr[bot_x][left-1] == ".":
+        for i in range(left-1,bot_y):
+            arr[bot_x][i]=arr[bot_x][i+1]
+        arr[bot_x][bot_y]="."
+
+def get_down_cc(cc_arr,a,b,c,d):
+    # if arr[a][b]=="#" or arr[c][d] == "#":
+    #     return cc_arr
+    if (c,d) >= (a,b):
+        cc_arr.append((a,b,c,d))
+    else:
+        cc_arr.append((c,d,a,b))
+        
+    if arr[a+1][b] == "[" and arr[c+1][d] == "]":
+        get_down_cc(cc_arr,a+1,b,c+1,d)
+    if arr[a+1][b] =="]":
+        get_down_cc(cc_arr,a+1,b-1,c+1,d-1)
+    if arr[a+1][d] == "[":
+        get_down_cc(cc_arr,a+1,b+1,c+1,d+1)
+    return cc_arr
+
+def shift_down(bot_x,bot_y,idx):
+    cc=[]
+    if arr[bot_x+1][bot_y]=="[":
+        cc = get_down_cc([],bot_x+1,bot_y,bot_x+1,bot_y+1)#
+    elif arr[bot_x+1][bot_y] == "]":
+        cc = get_down_cc([],bot_x+1,bot_y-1,bot_x+1,bot_y)
+    possible=True
+    # print("bot position",bot_x,bot_y)
+    if idx>727 and idx<731:
+        print("connected components: ",cc)
+    # cc = list(set(cc))
+
+    for i,j,k,l in cc:
+        if arr[i+1][j] == "#" or arr[k+1][l] == "#":
+            possible=False
+    movement_done=False
+    cc.sort()
+    if possible:
+        for i,j,k,l in reversed(cc):
+            arr[i+1][j]=arr[i][j]
+            arr[k+1][l]=arr[k][l]
+            arr[i][j]="."#
+            arr[k][l]="."#
+            movement_done=True
+    if possible and arr[bot_x+1][bot_y] == ".":
+        arr[bot_x+1][bot_y]=arr[bot_x][bot_y]
+        arr[bot_x][bot_y]="."
+        return
+    if movement_done:
+        arr[bot_x+1][bot_y]=arr[bot_x][bot_y]
+        arr[bot_x][bot_y]="."
+        
+def get_up_cc(cc_arr,a,b,c,d):
+    # if arr[a][b]=="#" or arr[c][d] == "#":
+    #     return cc_arr
+    # print("get_up_cc")
+    if (c,d) >= (a,b):
+        cc_arr.append((a,b,c,d))
+    else:
+        cc_arr.append((c,d,a,b))
+    if arr[a-1][b] == "[" and arr[c-1][d] == "]":
+        get_up_cc(cc_arr,a-1,b,c-1,d)
+    if arr[a-1][b] =="]":
+        get_up_cc(cc_arr,a-1,b-1,c-1,d-1)
+    if arr[a-1][d] == "[":
+        get_up_cc(cc_arr,a-1,b+1,c-1,d+1)
+    return cc_arr
+        
+def shift_up(bot_x,bot_y):
+    # print("shift_up")
+    cc=[]
+    if arr[bot_x-1][bot_y]=="[":
+        cc = get_up_cc([],bot_x-1,bot_y,bot_x-1,bot_y+1)
+    elif arr[bot_x-1][bot_y]=="]":
+        cc = get_up_cc([],bot_x-1,bot_y-1,bot_x-1,bot_y)
+    # print("cc:",cc)
+    # cc = list(set(cc))
+    possible=True
+    for i,j,k,l in cc:
+        if arr[i-1][j] == "#" or arr[k-1][l] == "#":
+            possible=False
+    movement_done=False
+    cc.sort()
+    if possible:
+        for i,j,k,l in reversed(cc):
+            arr[i-1][j]=arr[i][j]
+            arr[k-1][l]=arr[k][l]
+            arr[i][j]="."
+            arr[k][l]="."
+            movement_done=True
+    if possible and arr[bot_x-1][bot_y] == ".":
+        arr[bot_x-1][bot_y]=arr[bot_x][bot_y]
+        arr[bot_x][bot_y]="."
+        return
+    if movement_done:
+        arr[bot_x-1][bot_y]=arr[bot_x][bot_y]
+        arr[bot_x][bot_y]="."
+
+# print_arr()
+def count():
+    ct=0
+    for i in range(x):
+        for j in range(y):
+            if arr[i][j] == "[" and arr[i][j+1]=="]":
+                ct+=1
+    return ct
+
+idx=0
+for i in moves:    
+    if idx>727 and idx<731:
+        print("need to perform",i)
+    idx+=1
+    bot_x,bot_y=get_bot_pos()
+    if i == "^":
+        shift_up(bot_x,bot_y)
+    elif i == "v":
+        shift_down(bot_x,bot_y,idx)
+    elif i == ">":
+        shift_right(bot_x,bot_y)
+    elif i == "<":
+        shift_left(bot_x,bot_y)
+    
+    if idx>727 and idx<731:
+        print_arr()
+        print(count())
+        print()    
+
+# sum=0
+# for i in range(x):
+#     for j in range(y):
+#         if arr[i][j] == "[":
+#             sum+=100*i+j
+# print_arr()
+# print(sum)
+
+ct=0
+
+
+print(ct)
